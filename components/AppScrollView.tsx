7038f1369da1f6fa78344d4ec23a5c0ece001122
a <alejandrabermot@gmail.com>
2024-11-07 09:51:44 +0100

    Initial commit

--- components/AppScrollView.tsx
+++ components/AppScrollView.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren, ReactElement } from 'react';
-import { StyleSheet, useColorScheme } from 'react-native';
+import { StyleSheet, useColorScheme, Text } from 'react-native';
 import Animated, {
   interpolate,
   useAnimatedRef,
@@ -16,7 +16,7 @@ type Props = PropsWithChildren<{
   headerBackgroundColor: { dark: string; light: string };
 }>;
 
-export default function ParallaxScrollView({
+export default function AppScrollView({
   children,
   headerImage,
   headerBackgroundColor,
